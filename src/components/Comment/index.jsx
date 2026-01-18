6f2a0f7e3aa09356a3cbf23578e4e86f68450b5b
Bao An Tran <worldcoder1234@gmail.com>
2026-01-18 09:53:22 +0700

    feat:comment load more

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,15 +1,26 @@
 import { useEffect, useState } from "react";
 import api from "../../apis";
-import CommentBody from "../CommentBody";
 import CommentCard from "../CommentCard";
 const Comment = ({ videoId }) => {
   const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
-  const [commentList, setCommentList] = useState([]);
+  const [commentList, setCommentList] = useState({
+    comments: [],
+    nextPageToken: null,
+  });
   const fetchComments = async () => {
     const response = await api.get(
-      `commentThreads?part=snippet%2Creplies&maxResults=20&videoId=${videoId}&key=${API_KEY}`
+      `commentThreads?part=snippet%2Creplies&maxResults=20&videoId=${videoId}${
+        commentList.nextPageToken
+          ? `&pageToken=${commentList.nextPageToken}`
+          : ""
+      }&key=${API_KEY}`
     );
-    setCommentList(response.data.items);
+    console.log(response.data);
+
+    setCommentList((prev) => ({
+      comments: [...prev.comments, ...response.data.items],
+      nextPageToken: response.data.nextPageToken,
+    }));
   };
   useEffect(() => {
     fetchComments();
@@ -17,11 +28,13 @@ const Comment = ({ videoId }) => {
   return (
     <div>
       <h3 className="text-lg font-bold">Comments</h3>
-      {commentList.length > 0 &&
-        commentList.map((comment) => {
+      {commentList.comments.length > 0 &&
+        commentList.comments.map((comment) => {
           return <CommentCard key={comment.id} commentData={comment} />;
         })}
-      <button className="w-full p-2">Load more</button>
+      <button className="w-full p-2" onClick={() => fetchComments()}>
+        Load more
+      </button>
     </div>
   );
 };
