7f2a58cfb9ff087b7e3986730b8f0bcc7eea6619
Bao An Tran <worldcoder1234@gmail.com>
2026-03-12 18:38:01 +0700

    feat:done comment section

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -2,16 +2,25 @@ import { useEffect, useState } from "react";
 import api from "../../apis";
 import CommentBody from "../CommentBody";
 const Comment = ({ videoId }) => {
-  const [commentList, setCommentList] = useState([]);
+  const [commentList, setCommentList] = useState({
+    data: [],
+    nextPageToken: null,
+  });
   const fetchCommentList = async () => {
     const response = await api.get(
-      `commentThreads?part=snippet%2Creplies&maxResults=10&videoId=${videoId}&key=${
+      `commentThreads?part=snippet%2Creplies&maxResults=10&videoId=${videoId}${commentList.nextPageToken ? `&pageToken=${commentList.nextPageToken}` : ""}&key=${
         import.meta.env.VITE_YOUTUBE_API_KEY
       }`,
     );
     console.log(response.data);
 
-    setCommentList(response.data.items);
+    setCommentList((prev) => {
+      return {
+        data: [...prev.data, ...response.data.items],
+        nextPageToken: response.data.nextPageToken,
+      };
+    }); // cho 1 hàm callback để truy xuất lại vào state trước đó (previous state)
+    // reference
   };
   useEffect(() => {
     fetchCommentList();
@@ -20,10 +29,15 @@ const Comment = ({ videoId }) => {
     <div>
       <h3 className="text-lg font-bold">Comments</h3>
       <div className="flex flex-col gap-2">
-        {commentList.length > 0 &&
-          commentList.map((item) => {
+        {commentList.data.length > 0 &&
+          commentList.data.map((item) => {
             return <CommentBody key={item.id} commentData={item} />;
           })}
+      </div>
+      <div className="flex items-center justify-center">
+        <button className="p-2" onClick={() => fetchCommentList()}>
+          Load more ...
+        </button>
       </div>
     </div>
   );
